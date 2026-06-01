70e683770dbb6254db7b1c2e59c4d12647ebb6d9
User <user@example.com>
2026-06-01 23:35:25 +0300

    Add Vercel configuration

--- server/server.js
+++ server/server.js
@@ -41,4 +41,8 @@ app.get(/(.*)/, (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} with Local JSON Storage and Frontend Hosted`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT} with Local JSON Storage and Frontend Hosted`));
+}
+
+module.exports = app;
